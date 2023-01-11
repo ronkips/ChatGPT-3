1f18a5c21788feb1e005cf765938349c2e14c98d
ronkips <kipshillash@gmail.com>
2023-01-11 13:50:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+### Welcome 👋
+This is the starter template for the [build your own AI writing assistant w/ GPT-3] project.
+
+
---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
+
+const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="root">
+      <Head>
+        <title>GPT-3 writer</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Please, insert your headline here</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>insert your subtitle here</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="start typing here"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-writer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build by Hillary</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
